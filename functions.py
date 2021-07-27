97b3c5569b70d09219245b6afed4f1735f623c26
narku-coder <85505848+narku-coder@users.noreply.github.com>
2021-07-26 20:19:48 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -275,9 +275,11 @@ async def get_inventory(items, user):
    has_items = False
    usable_items = []
    for emp in items:
-     if emp['user_id'] == user.id and emp['amount'] > 0:
-       usable_items.append(emp['name'])
-       has_items = True
+     if emp['user_id'] == user.id:
+	      if "amount" in emp:
+		       if emp['amount'] > 0:
+			       usable_items.append(emp['name'])
+             has_items = True
    return has_items, usable_items
 
 async def activate_boost(boosts, user, num):
